1bb21d5316b48b85de4caf9fdd1f0ca127104875
Stanislaw M. Wasilewski <109121036+gitlaws@users.noreply.github.com>
2024-09-30 17:16:48 -0400

    Refactor home component layout and styles

--- src/app/common/home/home.component.ts
+++ src/app/common/home/home.component.ts
@@ -1,13 +1,22 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms'; // Import FormsModule
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterLink, CommonModule],
+  imports: [RouterLink, CommonModule, FormsModule], // Add FormsModule to imports
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
-export class HomeComponent {}
+export class HomeComponent {
+  selectedChatbot: any;
+  chatbots = [
+    { name: 'Gemini', description: 'Gemini AI chatbot description' },
+    {
+      name: 'Hugging Face',
+      description: 'Hugging Face AI chatbot description',
+    },
+  ];
+}
