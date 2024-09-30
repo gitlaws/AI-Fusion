4456d3da67866bbfd028ac7577bf7381b7953fa8
Stanislaw M. Wasilewski <109121036+gitlaws@users.noreply.github.com>
2024-09-30 15:53:38 -0400

    Refactor app configuration and routes
    - Update app.config.ts to provideZoneChangeDetection and provideRouter with proper formatting.
    - Update app.routes.ts to add a new route for the home component.
    - Update home.component.ts and toolbar.component.ts to import CommonModule and RouterLink.

--- src/app/common/toolbar/toolbar.component.ts
+++ src/app/common/toolbar/toolbar.component.ts
@@ -1,12 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-toolbar',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, CommonModule],
   templateUrl: './toolbar.component.html',
-  styleUrl: './toolbar.component.scss'
+  styleUrl: './toolbar.component.scss',
 })
-export class ToolbarComponent {
-
-}
+export class ToolbarComponent {}
