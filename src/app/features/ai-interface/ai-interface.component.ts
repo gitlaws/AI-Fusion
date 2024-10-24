ff23ae906aeda94f977f1faa3dd4cd39b29ef69c
Stanislaw M. Wasilewski <109121036+gitlaws@users.noreply.github.com>
2024-10-24 11:55:01 -0400

    updates

--- src/app/features/ai-interface/ai-interface.component.ts
+++ src/app/features/ai-interface/ai-interface.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ViewChild,
+  AfterViewInit,
+  Renderer2,
+  ElementRef,
+} from '@angular/core';
 
 @Component({
   selector: 'app-ai-interface',
   standalone: true,
   imports: [],
   templateUrl: './ai-interface.component.html',
-  styleUrl: './ai-interface.component.scss'
+  styleUrls: ['./ai-interface.component.scss'],
 })
-export class AiInterfaceComponent {
+export class AiInterfaceComponent implements AfterViewInit {
+  @ViewChild('inputField') inputField!: ElementRef;
 
+  constructor(private renderer: Renderer2) {}
+
+  ngAfterViewInit() {
+    this.focusInputField();
+  }
+
+  private focusInputField(): void {
+    if (this.inputField && this.inputField.nativeElement) {
+      this.renderer.selectRootElement(this.inputField.nativeElement).focus();
+    }
+  }
 }
