1027ab8013fecdbe0d07661a0a819f25b8b14692
Stanislaw M. Wasilewski <109121036+gitlaws@users.noreply.github.com>
2024-10-24 01:15:55 -0400

    added ai tools

--- src/app/features/ai-app/ai-app.component.ts
+++ src/app/features/ai-app/ai-app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { AiInterfaceComponent } from '../ai-interface/ai-interface.component';
+import { AiSelectionComponent } from '../ai-selection/ai-selection.component';
 
 @Component({
   selector: 'app-ai-app',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, AiInterfaceComponent, AiSelectionComponent],
   templateUrl: './ai-app.component.html',
   styleUrl: './ai-app.component.scss',
 })
--- src/app/features/ai-selection/ai-selection.component.ts
+++ src/app/features/ai-selection/ai-selection.component.ts
@@ -1,12 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-ai-selection',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule], // Add FormsModule to imports
   templateUrl: './ai-selection.component.html',
-  styleUrl: './ai-selection.component.scss'
+  styleUrl: './ai-selection.component.scss',
 })
 export class AiSelectionComponent {
-
+  selectedChatbot: any;
+  chatbots = [
+    { name: 'Gemini', description: 'Gemini AI chatbot description' },
+    {
+      name: 'Hugging Face',
+      description: 'Hugging Face AI chatbot description',
+    },
+  ];
 }
