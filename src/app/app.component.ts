88944c36196a2699b17dd95ab1dfc389c8b90d35
Stanislaw M. Wasilewski <109121036+gitlaws@users.noreply.github.com>
2024-10-24 00:17:12 -0400

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ToolbarComponent } from './common/toolbar/toolbar.component';
-import { MockToolbarComponent } from './common/mock-toolbar/mock-toolbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ToolbarComponent, MockToolbarComponent],
+  imports: [RouterOutlet, ToolbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
