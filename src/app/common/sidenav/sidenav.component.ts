5b681ea803dd110796e40899811ac33ac605f0d8
Staś <lawmski@gmail.com>
2024-10-27 07:23:57 -0400

    update

--- src/app/common/sidenav/sidenav.component.ts
+++ src/app/common/sidenav/sidenav.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-sidenav',
   standalone: true,
-  imports: [],
+  imports: [MatSidenavModule, MatListModule, MatIconModule, CommonModule],
   templateUrl: './sidenav.component.html',
-  styleUrl: './sidenav.component.scss'
+  styleUrls: ['./sidenav.component.scss'],
 })
 export class SidenavComponent {
+  menuItems = [
+    { name: 'Item 1', icon: 'home' },
+    { name: 'Item 2', icon: 'info' },
+    // Add more items as needed
+  ];
 
+  selectedItem: any;
+  sidenavOpened = false; // Control the state of the sidenav
+
+  selectItem(item: any) {
+    this.selectedItem = item;
+  }
+
+  toggleSidenav() {
+    this.sidenavOpened = !this.sidenavOpened;
+  }
 }
