7924ae6b238584c1784dda427b42d0d9cee4d6d3
Staś <lawmski@gmail.com>
2024-10-27 07:59:59 -0400

    added sidenav

--- src/app/common/sidenav/sidenav.component.ts
+++ src/app/common/sidenav/sidenav.component.ts
@@ -5,8 +5,16 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './sidenav.component.html',
-  styleUrl: './sidenav.component.scss'
+  styleUrl: './sidenav.component.scss',
 })
 export class SidenavComponent {
+  closeNav() {
+    // Logic to close the side navigation
+    console.log('Side navigation closed');
+  }
 
+  openNav() {
+    // Logic to open the side navigation
+    console.log('Side navigation opened');
+  }
 }
