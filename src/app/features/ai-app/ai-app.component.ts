e0080328031fbf33f371ce14019681ae17ced575
Staś <lawmski@gmail.com>
2024-10-27 05:41:50 -0400

    update

--- src/app/features/ai-app/ai-app.component.ts
+++ src/app/features/ai-app/ai-app.component.ts
@@ -2,11 +2,17 @@ import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { AiInterfaceComponent } from '../ai-interface/ai-interface.component';
 import { AiSelectionComponent } from '../ai-selection/ai-selection.component';
+import { ChatWindowComponent } from '../chat-window/chat-window.component';
 
 @Component({
   selector: 'app-ai-app',
   standalone: true,
-  imports: [RouterLink, AiInterfaceComponent, AiSelectionComponent],
+  imports: [
+    RouterLink,
+    AiInterfaceComponent,
+    AiSelectionComponent,
+    ChatWindowComponent,
+  ],
   templateUrl: './ai-app.component.html',
   styleUrl: './ai-app.component.scss',
 })
