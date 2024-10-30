632f41cfbf847a0785dbbf1511e4295becd6d75e
Staś <lawmski@gmail.com>
2024-10-30 13:36:16 -0400

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,14 +3,27 @@ import { RouterOutlet } from '@angular/router';
 import { ToolbarComponent } from './common/toolbar/toolbar.component';
 import { SidenavComponent } from './common/sidenav/sidenav.component';
 import { ChatBotComponent } from './features/chat-bot/chat-bot.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ToolbarComponent, SidenavComponent, ChatBotComponent],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    ToolbarComponent,
+    SidenavComponent,
+    ChatBotComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'AI-Fusion';
+
+  isSidenavOpen = false;
+
+  toggleSidenav() {
+    this.isSidenavOpen = !this.isSidenavOpen;
+  }
 }
--- src/app/common/sidenav/sidenav.component.ts
+++ src/app/common/sidenav/sidenav.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-sidenav',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './sidenav.component.html',
   styleUrl: './sidenav.component.scss',
 })
