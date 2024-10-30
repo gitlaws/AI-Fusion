8b2e056d72b562c03f6ba863d60ddca43cef0469
Staś <lawmski@gmail.com>
2024-10-30 04:36:05 -0400

    save

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,11 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
+    path: '',
+    redirectTo: 'ai-app',
+    pathMatch: 'full',
+  },
+  {
     path: 'home',
     loadComponent: () =>
       import('./common/home/home.component').then((m) => m.HomeComponent),
